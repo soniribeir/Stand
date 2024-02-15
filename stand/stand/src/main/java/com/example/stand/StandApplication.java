fecb9a7fad828258502700697af5c940fbbb38f5
soniribeir <soniribeiro@live.com.pt>
2024-02-15 10:49:03 +0000

    added springboot project

---
+++ stand/stand/src/main/java/com/example/stand/StandApplication.java
@@ -0,0 +1,13 @@
+package com.example.stand;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StandApplication.class, args);
+	}
+
+}
